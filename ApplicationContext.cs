6f39f210994dcf089bebd1531cd37cd0a15a2d1d
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-04-06 23:35:39 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace lab10
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=relationsdb20;Trusted_Connection=True;");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+
+
+namespace lab10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            static void Main(string[] args)
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    // пересоздаем базу данных
+                    db.Database.EnsureDeleted();
+                    db.Database.EnsureCreated();
+                    Company microsoft = new Company { Name = "Microsoft" };
+                    Company google = new Company { Name = "Google" };
+                    db.Companies.AddRange(microsoft, google);
+                    User tom = new User { Name = "Tom", Age = 36, Company = microsoft };
+                    User bob = new User { Name = "Bob", Age = 39, Company = google };
+                    User alice = new User { Name = "Alice", Age = 28, Company = microsoft };
+                    User kate = new User { Name = "Kate", Age = 25, Company = google };
+                    db.Users.AddRange(tom, bob, alice, kate);
+                    db.SaveChanges();
+                }
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    var users = db.Users.Where(u => u.Age > 30)
+                    .Intersect(db.Users.Where(u => u.Name.Contains("Tom")));
+                    foreach (var user in users)
+                        Console.WriteLine(user.Name);
+                }
+
+            }
+        }
+    }
+
+}
